a42b00d36da26eba217f016a12af3c9cbfa1c488
andre.snowman <andre@thedantas.com>
2020-01-17 11:20:46 -0300

    Feature(Pod install)
      pod 'SwiftLint'
      pod 'Alamofire'
      pod 'RealmSwift'
      pod 'RxSwift', '~> 5'
      pod 'RxCocoa', '~> 5'
      pod 'lottie-ios'
      pod 'SwiftyJSON'
      pod 'AlamofireObjectMapper', '~> 5.2'
      pod 'NVActivityIndicatorView'
      pod 'ObjectMapper', '~> 3.3'

---
+++ ioasys/ioasys/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ioasys
+//
+//  Created by Andre Costa Dantas on 17/01/20.
+//  Copyright © 2020 Andre Costa Dantas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
